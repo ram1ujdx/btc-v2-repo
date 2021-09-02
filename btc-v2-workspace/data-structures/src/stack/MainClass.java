649e24c942757d3124e890c6b963b5a2312c63b2
ramanujdas <ramanujdas@192.168.10.37>
2021-09-02 16:37:01 +0530

    Added Stack

---
+++ btc-v2-workspace/data-structures/src/stack/MainClass.java
@@ -0,0 +1,25 @@
+package stack;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Stack stack=new Stack(5);
+		
+		stack.push(10);
+		stack.push(15);
+		stack.push(5);
+		stack.push(20);
+		stack.push(50);
+		stack.push(30);
+		
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+
+	}
+
+}
