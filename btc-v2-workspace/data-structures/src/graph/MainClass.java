9ca0a667674f9e3ba13cd4a8d252c6efb033bb5b
ramanujdas <ramanujdas@localhost>
2021-09-22 16:13:40 +0530

    Graph Implementation

--- btc-v2-workspace/data-structures/src/graph/MainClass.java
+++ btc-v2-workspace/data-structures/src/graph/MainClass.java
@@ -4,18 +4,24 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		Graph graph=new Graph(5);
+		AdvancedGraph graph=new AdvancedGraph(5);
 		
-		graph.addEdge(0, 1);
-		graph.addEdge(0, 2);
-		graph.addEdge(0, 3);
-		graph.addEdge(1, 4);
-		graph.addEdge(1, 2);
-		graph.addEdge(2,3);
-		graph.addEdge(3, 1);
-		graph.addEdge(4, 0);
+		graph.addVertex("Rahul");
+		graph.addVertex("Rohit");
+		graph.addVertex("Mohan");
+		graph.addVertex("Priya");
+		graph.addVertex("Suraj");
 		
-		graph.getAdj(0).forEach(System.out::println);
+		
+		graph.addEdge("Rahul", "Rohit");
+		graph.addEdge("Rohit", "Priya");
+		graph.addEdge("Rahul", "Priya");
+		graph.addEdge("Mohan", "Rahul");
+		graph.addEdge("Suraj", "Priya");
+		graph.addEdge("Priya", "Suraj");
+		
+		
+		graph.printGraph();
 
 	}
 
