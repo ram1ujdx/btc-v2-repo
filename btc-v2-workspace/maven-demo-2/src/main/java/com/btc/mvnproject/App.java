267f334ca6f7aca50fd27f00ac2f7d593b984c50
Ramanuj Das <ramanujds9@gmail.com>
2021-10-08 12:44:04 +0530

    Added maven projects

---
+++ btc-v2-workspace/maven-demo-2/src/main/java/com/btc/mvnproject/App.java
@@ -0,0 +1,13 @@
+package com.btc.mvnproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ btc-v2-workspace/mvn-project-demo/src/main/java/com/btc/mvnproject/mvn_project_demo/App.java
@@ -0,0 +1,13 @@
+package com.btc.mvnproject.mvn_project_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
